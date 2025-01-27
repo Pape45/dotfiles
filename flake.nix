f7b990d442ab6ae219df78e6f6a3fed81bd0bed5
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2025-01-27 23:58:53 +0100

    Adding default dock apps

--- flake.nix
+++ flake.nix
@@ -50,6 +50,24 @@
         pkgs.nerd-fonts.fira-code
         pkgs.jetbrains-mono
       ];
+
+      # MacOs Settings
+      system.defaults = {
+        dock.persistent-apps = [
+          "/Applications/Safari.app"
+          "${pkgs.firefox-unwrapped}/Applications/Firefox.app"
+          "/System/Applications/Messages.app"
+          "/System/Applications/Mail.app"
+          "/System/Applications/Calendar.app"
+          "/System/Applications/Notes.app"
+          "/System/Applications/TV.app"
+          "${pkgs.vscode}/Applications/Visual Studio Code.app"
+          "/System/Applications/App Store.app"
+          "/System/Applications/System Settings.app"
+          "${pkgs.obsidian}/Applications/Obsidian.app"
+          "/System/Applications/Utilities/Terminal.app"
+        ];
+      };
       
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
