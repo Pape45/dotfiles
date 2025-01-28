34fb0fa1e849ca93da79afadffcd392c17426530
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2025-01-28 18:33:11 +0100

    Small changes

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -2,7 +2,7 @@
   system.defaults.finder.FXPreferredViewStyle = "Nlsv";
   nix.settings.experimental-features = "nix-command flakes";
   programs.zsh.enable = true;
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+  system.configurationRevision = null;
   system.stateVersion = 5;
   nixpkgs.hostPlatform = "aarch64-darwin";
 }
