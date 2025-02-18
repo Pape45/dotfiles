9b8cfb1288fe2adacde5641cc50a586ee8c96a3f
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2025-02-18 15:22:29 +0100

    Removed firefox from dock

--- nix-config/modules/apps/dock.nix
+++ nix-config/modules/apps/dock.nix
@@ -2,12 +2,10 @@
   system.defaults.dock.persistent-apps = [
     "/System/Applications/Launchpad.app" 
     "/Applications/Safari.app"
-    "${pkgs.firefox-unwrapped}/Applications/Firefox.app"
     "/System/Applications/Messages.app"
     "/System/Applications/Mail.app"
     "/System/Applications/Calendar.app"
     "/System/Applications/Notes.app"
-    "/System/Applications/TV.app"
     "${pkgs.vscode}/Applications/Visual Studio Code.app"
     "/System/Applications/App Store.app"
     "/System/Applications/System Settings.app"
